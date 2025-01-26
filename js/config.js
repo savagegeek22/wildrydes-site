105060e27ceaa08943800df396238f4cb6e8feb1
savagegeek22 <56052584+savagegeek22@users.noreply.github.com>
2025-01-26 11:07:14 -0500

    Update config.js
    Added userPoodID and userPoolClientID

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_HtWl4JLnh', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '10amml5um05ja2j1b7kdd00i4d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
